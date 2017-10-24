bfb29688e1602b77460b34bb058029c661f3fb84
Kevin Migueis <kmigueis1@gmail.com>
2017-10-24 12:28:02 -0400

    Add static pages for root div and user json views

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,21 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login(@user)
+      render '/api/users/show'
+    else
+      render json: ["Invalid username/password combination"], status: 401
+    end
+  end
+
+  def destroy
+    if !current_user
+      render json: ["Not logged in"], status: 404
+    else
+      logout
+      render json: {}
+    end
+  end
+end
