08cd0827660b4694ec6d068d70b886f18383b526
Kevin Migueis <kmigueis1@gmail.com>
2017-10-25 10:12:00 -0400

    Add protected routes to authentication

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
 import {Route} from 'react-router-dom';
 import SessionFormContainer from './session/session_form_container';
-// import { AuthRoute } from '../util/route_util';
+import { AuthRoute } from '../util/route_util';
 const App = () => (
   <div>
     <header>
@@ -10,8 +10,8 @@ const App = () => (
       <GreetingContainer/>
     </header>
 
-    <Route path='/login' component={SessionFormContainer} />
-    <Route path='/signup' component={SessionFormContainer} />
+    <AuthRoute path='/login' component={SessionFormContainer} />
+    <AuthRoute path='/signup' component={SessionFormContainer} />
   </div>
 );
 
