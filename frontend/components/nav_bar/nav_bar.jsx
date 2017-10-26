033f51813f2fb1421e54ea06826785e2b99c18ea
Kevin Migueis <kmigueis1@gmail.com>
2017-10-26 12:18:07 -0400

    Adjust homebutton size

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -7,8 +7,10 @@ import AccountBar from '../account_bar/account_bar';
 const NavBar = () => (
   <div className="nav-bar">
     <div className='left-nav'>
-      <div className="home-button">
-
+      <div className="home-button-container">
+        <div  className="home-button">
+          
+        </div>
       </div>
       <SearchBar />
     </div>
