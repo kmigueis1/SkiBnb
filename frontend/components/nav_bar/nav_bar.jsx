50bf6ef94a69e22142df697e2cb57200052a06fd
Kevin Migueis <kmigueis1@gmail.com>
2017-10-28 21:45:25 -0400

    Wrap home icon in link tag that links to homes index

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -3,14 +3,17 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import SearchBar from '../search_bar/search_bar';
 import AccountBar from '../account_bar/account_bar';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => (
   <div className="nav-bar">
     <div className='left-nav'>
       <div className="home-button-container">
-        <div  className="home-button">
-          
-        </div>
+        <Link to='/' className="home-button-link">
+          <div  className="home-button">
+          </div>    
+        </Link>
+
       </div>
       <SearchBar />
     </div>
