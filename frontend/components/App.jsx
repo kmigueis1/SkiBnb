4fe3fe1b10d61dbe568ccbc92e6de736349ceb51
Kevin Migueis <kmigueis1@gmail.com>
2017-10-29 10:53:42 -0400

    Add switch from react router to separate homes index and user edit paths

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
-import {Route} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import SessionFormContainer from './session/session_form_container';
 import { AuthRoute, UserRoute } from '../util/route_util';
 import NavBar from './nav_bar/nav_bar';
@@ -17,11 +17,11 @@ const App = () => (
 
     <AuthRoute path='/login' component={SessionFormContainer} />
     <AuthRoute path='/signup' component={SessionFormContainer} />
-    <UserRoute path='/user/edit' component={UserEditFormContainer} />
-
     <div className="main-content">
-      <HomesIndexContainer />
-
+      <Switch>        
+        <UserRoute path='/user/edit' component={UserEditFormContainer} />
+        <Route path='/' component={HomesIndexContainer} />
+      </Switch>
     </div>
   </div>
 );
