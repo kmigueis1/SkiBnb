5a4dfe47d114a60ba6a9ca0f5c0f48eea1c1facc
Kevin Migueis <kmigueis1@gmail.com>
2017-10-31 15:29:38 -0400

    Style booking index page

--- app/views/api/bookings/show.json.jbuilder
+++ app/views/api/bookings/show.json.jbuilder
@@ -1,8 +1,8 @@
 json.id @booking.id
-json.startDate @booking.start_date
-json.endDate @booking.end_date
-json.userId @booking.user_id
-json.homeId @booking.home_id
+json.start_date @booking.start_date
+json.end_date @booking.end_date
+json.user_id @booking.user_id
+json.home_id @booking.home_id
 
 
 # $.ajax({
--- frontend/components/booking/booking_item.jsx
+++ frontend/components/booking/booking_item.jsx
@@ -6,18 +6,18 @@ class BookingItem extends React.Component{
     console.log(props);
 
     this.months = {
-      "01": "Jan",
-      "02": "Feb",
-      "03": "Mar",
-      "04": "Apr",
-      "05": "May",
-      "06": "Jun",
-      "07": "Jul",
-      "08": "Aug",
-      "09": "Sep",
-      "10": "Oct",
-      "11": "Nov",
-      "12": "Dec"
+      1: "Jan",
+      2: "Feb",
+      3: "Mar",
+      4: "Apr",
+      5: "May",
+      6: "Jun",
+      7: "Jul",
+      8: "Aug",
+      9: "Sep",
+      10: "Oct",
+      11: "Nov",
+      12: "Dec"
     }
   }
 
@@ -25,23 +25,21 @@ class BookingItem extends React.Component{
     let booking = this.props.booking;
     let startDate = booking.start_date.split("-");
     let startDateYear = startDate[0];
-    let startDateMonth = startDate[1];
+    let startDateMonth = parseInt(startDate[1]);
     let startDateDay = startDate[2];
 
     let endDate = booking.end_date.split("-");
     let endDateYear = endDate[0];
-    let endDateMonth = endDate[1];
+    let endDateMonth = parseInt(endDate[1]);
     let endDateDay = endDate[2];
 
-
-    console.log(startDate);
     return(
       <div className="booking-item">
         <div className="booking-photo"><img src={booking.image_url}></img></div>
         <div className="booking-summary">
           <div className="booking-title"><span>{booking.title}</span></div>
           <div className="booking-dates">
-            <span>`${this.months[startDateMonth]} ${startDateDay} - ${this.month[endDateMonth]} ${endDateDay}, endDateYear`</span>
+            <span>{this.months[startDateMonth]} {startDateDay} - {this.months[endDateMonth]} {endDateDay}, {endDateYear}</span>
           </div>
         </div>
       </div>
