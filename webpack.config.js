3f37cf8dff220e74eae2655d2807697381e920a3
Kevin Migueis <kmigueis1@gmail.com>
2017-11-21 00:19:08 -0500

    Remove webpack plugin as it prevents pushing to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -23,9 +23,4 @@ module.exports = {
     ]
   },
   devtool: 'source-map',
-  plugins: [
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-  }),
-]
 };
