b3cf9c1a9f06b611005b6e28136e5f85279b0152
Kevin Migueis <kmigueis1@gmail.com>
2017-11-20 23:37:49 -0500

    Add favicon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
 
   helper_method :logged_in?, :current_user
 
+  config.relative_url_root = ""
+
   def login(user)
     session[:session_token] = user.reset_session_token!
     @current_user = user
